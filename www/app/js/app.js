952816bebe01b4f2fffad3ee0bf4b99068c632b0
bcowzer <bcowzer@hotmail.com>
2014-05-20 11:07:59 +0100

    add files

---
+++ www/app/js/app.js
@@ -0,0 +1,63 @@
+var app = angular.module("app", ['ngRoute'])
+
+app.config(function($routeProvider) {
+
+  $routeProvider.when('/login', {
+    templateUrl: 'login.html',
+    controller: 'LoginController'
+  });
+
+  $routeProvider.when('/home', {
+    templateUrl: 'home.html',
+    controller: 'cameraController'
+  });
+
+  $routeProvider.otherwise({ redirectTo: '/login' });
+
+});
+
+app.factory("AuthenticationService", function($location) {
+  return {
+    login: function(credentials) {
+      if (credentials.username !== "guest" || credentials.password !== "1234") {
+        alert("Username or password are incorrect - try again");
+      } else {
+        $location.path('/home');
+      }
+    }
+  };
+});
+
+app.controller("LoginController", function($scope, $location, AuthenticationService) {
+  $scope.credentials = { username: "", password: "" };
+
+  $scope.login = function() {
+    AuthenticationService.login($scope.credentials);
+  }
+});
+/* from https://github.com/hollyschinsky/MyAngularPhoneGapProject/blob/master/www/js/controllers.js */
+
+ app.controller("cameraController", function($scope) {
+    $scope.takePic = function() {
+        var options = {
+            quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: 1, // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+            encodingType: 0 // 0=JPG 1=PNG
+        }
+        // Take picture using device camera and retrieve image as base64-encoded string
+        navigator.camera.getPicture(onSuccess,onFail,options);
+    }
+    var onSuccess = function(imageData) {
+        console.log("On Success! ");
+        $scope.picData = "data:image/jpeg;base64," +imageData;
+        $scope.$apply();
+    };
+    var onFail = function(e) {
+        console.log("On fail " + e);
+    };
+}
+
+
+
+
