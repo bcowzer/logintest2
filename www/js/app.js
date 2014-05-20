4c3be6a510640e069417b87953a5085e851a4efc
bcowzer <bcowzer@hotmail.com>
2014-05-20 14:59:40 +0100

    edit files

---
+++ www/js/app.js
@@ -0,0 +1,44 @@
+var app = angular.module("app", ['ngRoute'])
+
+app.config(function($routeProvider) {
+
+  $routeProvider.when('/login', {
+    templateUrl: 'views/login.html',
+    controller: 'LoginController'
+  });
+
+  $routeProvider.when('/home', {
+    templateUrl: 'views/home1.html',
+    controller: 'cameraController'
+  });
+
+  $routeProvider.otherwise({ redirectTo: '/login' });
+
+});
+
+app.factory("AuthenticationService", function($location) {
+  return {
+    login: function(credentials) {
+      if (credentials.username !== "guest" || credentials.password !== "1234") {
+        alert("Username or password are incorrect - try again");
+      } else {
+        $location.path('/home');
+      }
+    }
+  };
+});
+
+app.controller("LoginController", function($scope, $location, AuthenticationService) {
+  $scope.credentials = { username: "", password: "" };
+
+  $scope.login = function() {
+    AuthenticationService.login($scope.credentials);
+  }
+});
+/* from https://github.com/hollyschinsky/MyAngularPhoneGapProject/blob/master/www/js/controllers.js */
+
+
+
+
+
+
