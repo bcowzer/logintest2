b4ffd1a1807a3fa0de40c7f8264feae061af6f94
bcowzer <bcowzer@hotmail.com>
2014-06-03 11:56:07 +0100

    edit

--- www/js/app.js
+++ www/js/app.js
@@ -60,14 +60,14 @@ app.controller("LoginController", function($scope, $location, AuthenticationServ
     $scope.takePic = function() {
          
       
-        navigator.camera.getPicture( onSuccess, onFail, {
+        navigator.camera.getPicture(onSuccess,onFail, {
             quality: 50, 
             destinationType: Camera.DestinationType.FILE_URI,
-            saveToPhotoAlbum: true
+            saveToPhotoAlbum: true;
         })
     }
             function onSuccess(imageData) {
-                $scope.picData = "something";
+                $scope.picData = imageData;
                 $scope.$apply();
     };
     var onFail = function(e) {
